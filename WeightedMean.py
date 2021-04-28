cd4fb2665c6b882fa534aeeba3e7a86ec3aa517f
pallabichakraborty <pallavichakraborty@yahoo.com>
2021-04-28 10:42:07 -0500

    Create WeightedMean.py

---
+++ WeightedMean.py
@@ -0,0 +1,35 @@
+/*
+Tutorial: https://www.khanacademy.org/computing/pixar/environment-modeling-2/mathematics-of-parabolas2-ver2/v/weighted-average-two-points
+ */
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'weightedMean' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY X
+#  2. INTEGER_ARRAY W
+#
+
+def weightedMean(X, W):
+    # Write your code here
+    total=0
+    for i in range(len(weights)):
+        total+=weights[i]*vals[i]
+    print(round(total/sum(weights),1))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    vals = list(map(int, input().rstrip().split()))
+
+    weights = list(map(int, input().rstrip().split()))
+
+    weightedMean(vals, weights)
