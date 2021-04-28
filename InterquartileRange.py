8196fb5426e7abf1b4a2fd7f26fc92ab4afeb003
pallabichakraborty <pallavichakraborty@yahoo.com>
2021-04-28 11:55:40 -0500

    Create InterquartileRange.py

---
+++ InterquartileRange.py
@@ -0,0 +1,11 @@
+/*
+Tutorial: https://www.khanacademy.org/math/statistics-probability/summarizing-quantitative-data/interquartile-range-iqr/a/interquartile-range-review?modal=1
+
+Here's how to find the IQR:
+Step 1: Put the data in order from least to greatest.
+Step 2: Find the median. If the number of data points is odd, the median is the middle data point. If the number of data points is even, the median is the average of the middle two data points.
+Step 3: Find the first quartile (Q1). The first quartile is the median of the data points to the left of the median in the ordered list.
+Step 4: Find the third quartile (Q3(. The third quartile is the median of the data points to the right of the median in the ordered list.
+Step 5: Calculate IQR by subtracting Q3-Q1
+
+*/
