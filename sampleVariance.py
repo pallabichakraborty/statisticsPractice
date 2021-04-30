cc2e8a1dc16053d4f8458c5113bf1dc2a052e950
pallabichakraborty <pallavichakraborty@yahoo.com>
2021-04-30 10:47:29 -0500

    Added Code for Sample Variance

--- sampleVariance.py
+++ sampleVariance.py
@@ -4,12 +4,25 @@ Compute Sample Variance
 
 #!/bin/python3
 
-def sampleVariance(list):
-  return
+import statistics
+
+def sampleVariance(list, listitemcount):
+    mean=statistics.mean(list)
+    
+    sampleVariance=0
+    
+    for item in list:
+        sampleVariance+=(item - mean) **2
+        
+    sampleVariance=sampleVariance/(listitemcount-1)
+    
+    print("Sample Variance : {}".format(sampleVariance))
+    
+    return sampleVariance
 
 if __name__ == '__main__':
     n = int(input().strip())
 
     vals = list(map(int, input().rstrip().split()))
     
-    sampleVariance(vals)
+    sampleVariance(vals, n)
