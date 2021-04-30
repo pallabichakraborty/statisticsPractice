175405e2cc1133f18fd4e869f298393eb244a279
pallabichakraborty <pallavichakraborty@yahoo.com>
2021-04-30 10:38:54 -0500

    Update and rename sampleVariance to sampleVariance.py

---
+++ sampleVariance.py
@@ -0,0 +1,15 @@
+/*
+Compute Sample Variance
+*/
+
+#!/bin/python3
+
+def sampleVariance(list):
+  return
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    vals = list(map(int, input().rstrip().split()))
+    
+    sampleVariance(vals)
